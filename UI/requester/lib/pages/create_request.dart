d66a7e93467b0984073efe150afd10edaaedd193
kimsj1993 <kimsj1993@gmail.com>
2020-02-03 02:01:19 -0600

    added appBar on createRequest page so that it can go back to home page

--- UI/requester/lib/pages/create_request.dart
+++ UI/requester/lib/pages/create_request.dart
@@ -9,6 +9,13 @@ class _CreateRequestState extends State<CreateRequest> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+        backgroundColor: Colors.grey[200],
+        appBar: AppBar(
+          backgroundColor: Colors.blue[900],
+          title: Text('Create a Request'),
+          centerTitle: true,
+          elevation: 0,
+        ),
         body: Text('Create request page'),
     );
   }
