737c4011f580c6d893cbfb5629543e8d42e92965
kimsj1993 <kimsj1993@gmail.com>
2020-02-10 02:23:10 -0600

    added add board screen

--- UI/requester/lib/screens/add_board_screen.dart
+++ UI/requester/lib/screens/add_board_screen.dart
@@ -7,6 +7,8 @@ class AddBoardScreen extends StatefulWidget {
 }
 
 class _AddBoardScreenState extends State<AddBoardScreen> {
+  final _requestController = TextEditingController();
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -14,12 +16,31 @@ class _AddBoardScreenState extends State<AddBoardScreen> {
         title: Text('Add a new request'),
       ),
       body: Column(
+        crossAxisAlignment: CrossAxisAlignment.stretch,
         children: <Widget>[
-          Text('User Inputss..'),
+          Expanded(
+            child: SingleChildScrollView(
+              child: Padding(
+                padding: const EdgeInsets.all(10.0),
+                child: Column(
+                  children: <Widget>[
+                    TextField(
+                      decoration: InputDecoration(
+                          labelText: 'What do you want to Request?'),
+                      controller: _requestController,
+                    ),
+                  ],
+                ),
+              ),
+            ),
+          ),
           RaisedButton.icon(
             icon: Icon(Icons.add),
             label: Text('Add Request'),
             onPressed: () {},
+            elevation: 0,
+            materialTapTargetSize: MaterialTapTargetSize.shrinkWrap,
+            color: Theme.of(context).accentColor,
           ),
         ],
       ),
