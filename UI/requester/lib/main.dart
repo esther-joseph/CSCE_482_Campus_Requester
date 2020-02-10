d31230561792bf32428737ba974e2d8c1ae8319f
kimsj1993 <kimsj1993@gmail.com>
2020-02-10 01:44:21 -0600

    added provider

--- UI/requester/lib/main.dart
+++ UI/requester/lib/main.dart
@@ -1,17 +1,24 @@
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+import 'package:requester/screens/board_list_screen.dart';
+
+import './providers/Boards.dart';
 
 void main() => runApp(MyApp());
 
 class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Requester',
-      theme: ThemeData(
-        primaryColor: Colors.indigo,
-        accentColor: Colors.amber,
+    return ChangeNotifierProvider(
+      create: (ctx) => new Boards(),
+          child: MaterialApp(
+        title: 'Requester',
+        theme: ThemeData(
+          primaryColor: Colors.indigo,
+          accentColor: Colors.amber,
+        ),
+        home: BoardListScreen()
       ),
-      home: ...,
     )
   }
 
--- UI/requester/lib/providers/Boards.dart
+++ UI/requester/lib/providers/Boards.dart
@@ -1,3 +1,11 @@
 import 'package:flutter/material.dart';
 
-class Boards with ChangeNotifier {}
+import './Board.dart';
+
+class Boards with ChangeNotifier {
+  List<Board> _items = [];
+
+  List<Board> get items {
+    return [..._items];
+  }
+}
