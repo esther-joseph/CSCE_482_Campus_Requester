6f6e9a211fa91e422f3e05f5b0064b39fff2c104
kimsj1993 <kimsj1993@gmail.com>
2020-02-10 02:34:00 -0600

    update ui

--- UI/requester/lib/screens/auth-screen.dart
+++ UI/requester/lib/screens/auth-screen.dart
@@ -16,19 +16,6 @@ class AuthScreen extends StatelessWidget {
       // resizeToAvoidBottomInset: false,
       body: Stack(
         children: <Widget>[
-          Container(
-            decoration: BoxDecoration(
-              gradient: LinearGradient(
-                colors: [
-                  Color.fromRGBO(215, 117, 255, 1).withOpacity(0.5),
-                  Color.fromRGBO(255, 188, 117, 1).withOpacity(0.9),
-                ],
-                begin: Alignment.topLeft,
-                end: Alignment.bottomRight,
-                stops: [0, 1],
-              ),
-            ),
-          ),
           SingleChildScrollView(
             child: Container(
               height: deviceSize.height,
