1d07b486e8773959827d9e0302fa15bef07af819
kimsj1993 <kimsj1993@gmail.com>
2020-02-10 02:51:58 -0600

    added simple singup logic

--- UI/requester/lib/main.dart
+++ UI/requester/lib/main.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
+import 'package:requester/providers/auth.dart';
 
 import './providers/Boards.dart';
 import 'package:requester/screens/board_list_screen.dart';
@@ -12,18 +13,21 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return ChangeNotifierProvider(
-      create: (ctx) => new Boards(),
-      child: MaterialApp(
-        title: 'Requester',
-        theme: ThemeData(
-          primaryColor: Colors.indigo,
-          accentColor: Colors.amber,
+      create: (ctx) => new Auth(),
+      child: ChangeNotifierProvider(
+        create: (ctx) => new Boards(),
+        child: MaterialApp(
+          title: 'Requester',
+          theme: ThemeData(
+            primaryColor: Colors.indigo,
+            accentColor: Colors.amber,
+          ),
+          home: AuthScreen(),
+          routes: {
+            AddBoardScreen.routeName: (ctx) => AddBoardScreen(),
+            AuthScreen.routeName: (ctx) => AuthScreen(),
+          },
         ),
-        home: AuthScreen(),
-        routes: {
-          AddBoardScreen.routeName: (ctx) => AddBoardScreen(),
-          AuthScreen.routeName: (ctx) => AuthScreen(),
-        },
       ),
     );
   }
---
+++ UI/requester/lib/providers/auth.dart
@@ -0,0 +1,19 @@
+import 'dart:convert';
+import 'package:flutter/material.dart';
+import 'package:http/http.dart' as http;
+
+class Auth with ChangeNotifier {
+  String _token;
+  DateTime _expiryDate;
+  String _userId;
+
+  Future<void> signup(String email, String password) async {
+    final url = '';
+
+    /*TODO
+    Have to add url
+    */
+    final response = await http.post(url,
+        body: jsonEncode({email: email, password: password}));
+  }
+}
