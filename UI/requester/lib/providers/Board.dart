97e211477d35f81a58bedc1925e8d231b09b9627
kimsj1993 <kimsj1993@gmail.com>
2020-02-10 01:37:08 -0600

    add location instance to board

--- UI/requester/lib/providers/Board.dart
+++ UI/requester/lib/providers/Board.dart
@@ -1,14 +1,24 @@
 import 'package:flutter/foundation.dart';
 
+class PlaceLocation {
+  final double latitude;
+  final double longitude;
+  final String address;
+
+  PlaceLocation({this.address, this.latitude, this.longitude});
+}
+
 class Board {
   final String id;
   final String details;
   final String creationTime;
+  final PlaceLocation location;
   bool isOpen;
 
   Board(
       {@required this.id,
       @required this.details,
       @required this.creationTime,
+      this.location,
       @required this.isOpen});
 }
